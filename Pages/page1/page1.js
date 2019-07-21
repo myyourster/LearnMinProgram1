55af59de020d36a8201f960c6a35e8761160b12d
myster <2729697142@qq.com>
2019-07-21 23:48:34 +0800

    小程序初体验

--- Pages/page1/page1.js
+++ Pages/page1/page1.js
@@ -1,66 +1,26 @@
-// Pages/page1/page1.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    name:'Coderwhy',
+    age: 18,
+    student:[
+      { id: 100, name:'kobe',age:30},
+      { id: 101, name: 'wayi', age: 32 },
+    ],
+    counter:0
 
   },
+  handBtnClick(){
+    // 1.错误做法。界面不会刷新
+  //  this.data.counter += 1
+  //  console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  this.setData ({
+    counter: this.data.counter +=1
+  })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handSub() {
+    this.setData({
+      counter: this.data.counter -= 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
